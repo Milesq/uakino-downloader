dd3d8c31b43ac965792b3478590d7a0dc406d388
Milesq <wmilosz88@gmail.com>
2023-08-07 14:02:59 +0200

    feat(app): migrate merger to cloud

--- app/merge.py
+++ app/merge.py
@@ -1,15 +1,14 @@
-from os import listdir, system
+from os import listdir
 
-from movies import movie
 from utils import percent
 
-files = listdir(movie['name'])
+def merge_files(movie):
+  files = listdir(movie['name'])
 
-files = sorted(files, key=lambda x: int(x.split('.')[0]))
+  files = sorted(files, key=lambda x: int(x.split('.')[0]))
 
-with open(movie['name'] + '.ts', 'wb') as out:
+  with open(movie['name'] + '.ts', 'wb') as out:
     for i, filename in enumerate(files):
-        system('cls')
-        print(percent(i, len(files)), '%', sep='')
-        with open(f'{movie["name"]}/{filename}', 'rb') as f:
-            out.write(f.read())
+      print(percent(i, len(files)), '%', sep='')
+      with open(f'{movie["name"]}/{filename}', 'rb') as f:
+        out.write(f.read())
