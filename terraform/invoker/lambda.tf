6392f9f2015d057b8e16e8fb31a6be6c1688c5e9
Milesq <wmilosz88@gmail.com>
2023-08-07 13:31:49 +0200

    feat(invoker): add node_modules layer & basic env variables

--- terraform/invoker/lambda.tf
+++ terraform/invoker/lambda.tf
@@ -1,7 +1,8 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.cwd}/../invoker"
-  output_path = "./downloader.zip"
+  output_path = "./invoker.zip"
+  excludes    = ["node_modules", "node_modules.zip"]
 }
 
 resource "aws_lambda_function" "invoker" {
@@ -13,6 +14,27 @@ resource "aws_lambda_function" "invoker" {
 
   runtime = "nodejs18.x"
   handler = "main.handler"
+
+  environment {
+    variables = {
+      "ACCESS_PASSWORD" = random_password.user.result
+      "NODE_PATH" = "/opt/node_modules"
+      "NODE_ENV" = "production"
+    }
+  }
+
+  layers = [aws_lambda_layer_version.node_modules.arn]
+}
+
+locals {
+  layer_zip = "${path.cwd}/../invoker/node_modules.zip"
+}
+
+resource "aws_lambda_layer_version" "node_modules" {
+  layer_name          = "uakino_invoker_node_modules"
+  filename            = local.layer_zip
+  source_code_hash    = filebase64sha256(local.layer_zip)
+  compatible_runtimes = ["nodejs18.x"]
 }
 
 resource "aws_lambda_function_url" "url" {
